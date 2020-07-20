9dde0a5a2f90bb39ad5ae35e477e6e3bfb8f59d9
Anatooliy <Anatooliy@ukr.net>
2020-07-20 23:14:06 +0300

    added resipes module

--- RecipeBookUI/src/app/app-routing.module.ts
+++ RecipeBookUI/src/app/app-routing.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RecipesComponent } from './recipes/recipes.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent, children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipeEditComponent },
-        { path: ':id', component: RecipeDetailComponent },
-        { path: ':id/edit', component: RecipeEditComponent }
-    ] },
+    { path: '', redirectTo: 'recipes', pathMatch: 'full' }
+    /*,
     { path: 'page-not-found', component: PageNotFoundComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'page-not-found' }
+    { path: '**', pathMatch: 'full', redirectTo: 'page-not-found' }*/
 ];
 
 export const appRouting = RouterModule.forRoot(routes);
@@ -25,6 +16,4 @@ export const appRouting = RouterModule.forRoot(routes);
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- RecipeBookUI/src/app/app.module.ts
+++ RecipeBookUI/src/app/app.module.ts
@@ -4,37 +4,27 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { HeaderComponent } from './header/header.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recepi.service';
+import { RecipesModule } from './recipes/recipes.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     HeaderComponent,
     DropdownDirective,
-    PageNotFoundComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecipesModule
   ],
   providers: [RecipeService],
   bootstrap: [AppComponent]
